fbb94706e61381551efd7a4b46e083dbd5cb5344
monty1689 <work.manojkaushik@gmail.com>
2025-03-25 16:17:27 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["public.readdy.ai"], // Add your external image domain here
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
